97b4d97f280c0a539509f9551d71f3e7cb4d483b
victor-alegre-santos <victorvkr98@gmail.com>
2020-04-01 11:36:22 +0200

    Añadidos ejemplos y datos

---
+++ SQL.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+
+
+def create_spark_session():
+    MASTER = 'local[3]'
+    return SparkSession.builder.master(MASTER).appName('SQL app').getOrCreate()
+
+
+if __name__ == '__main__':
+    spark = create_spark_session()
+
+    ##
+    # data_frame = spark.read.json('personas.json')
+    # data_frame.show()
+
+    ##
+    # data_frame.select(data_frame['nombre'], data_frame['edad'][:1] + '0').show()
+
+    ##
+    df = spark.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('train.csv')
+
+    spark.stop()
